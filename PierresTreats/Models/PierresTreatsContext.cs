4e764cd2214a309bd0dee464fe8f6828809fc2ad
Dani Renner <danijrenner@gmail.com>
2021-03-26 14:37:01 -0700

    rename treat table to treats

--- PierresTreats/Models/PierresTreatsContext.cs
+++ PierresTreats/Models/PierresTreatsContext.cs
@@ -5,7 +5,7 @@ namespace PierresTreats.Models
 {
   public class PierresTreatsContext : IdentityDbContext<ApplicationUser>
   {
-    public virtual DbSet<Treat> Categories { get; set; }
+    public virtual DbSet<Treat> Treats { get; set; }
     public DbSet<Flavor> Flavors { get; set; }
     public DbSet<FlavorTreat> FlavorTreat { get; set; }
 
