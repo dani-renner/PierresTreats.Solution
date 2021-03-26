dabc973116079fd41eddfe114d5d22eb606f969f
Dani Renner <danijrenner@gmail.com>
2021-03-26 15:02:36 -0700

    fix some spacing

--- PierresTreats/Models/Flavor.cs
+++ PierresTreats/Models/Flavor.cs
@@ -12,7 +12,6 @@ namespace PierresTreats.Models
     public int FlavorId { get; set; }
     public string Name { get; set; }
     public virtual ApplicationUser User { get; set; }
-
     public virtual ICollection<FlavorTreat> JoinEntities { get;}
   }
 }
